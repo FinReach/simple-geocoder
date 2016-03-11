f8dce0a49159977dfcc76ff0c50fbbdce499fce1
altmer <igendou@gmail.com>
2016-03-11 15:20:25 +0100

    Version 0.0.2 - Austria added

--- lib/simple/geocoder.rb
+++ lib/simple/geocoder.rb
@@ -6,10 +6,11 @@ module Simple
   module Geocoder
 
     class Service
-      attr_accessor :germany, :countries
+      attr_accessor :germany, :countries, :austria
 
       def initialize
         self.germany = Simple::Geocoder::Germany.new
+        self.austria = Simple::Geocoder::Austria.new
         self.countries = Simple::Geocoder::Countries.new
       end
 
@@ -17,20 +18,23 @@ module Simple
         postal_code = postal_code.to_s
         if country_code.present?
           country_code = country_code.upcase
-          if country_code == 'DE'
-            # search in German postal codes
-            res = self.germany.geocode(postal_code)
-          else
-            # search in countries centroids
-            res = self.countries.geocode(country_code)
+          case country_code
+            when 'DE'
+              res = self.germany.geocode(postal_code)
+              # if nothing found, then it could be post box
+              # try to search by city
+              res = self.germany.geocode(city_name) if res.blank?
+            when 'AT'
+              res = self.austria.geocode(postal_code)
+              res = self.austria.geocode(city_name) if res.blank?
+            else
+              # search in countries centroids
+              res = self.countries.geocode(country_code)
           end
         else
           # try to search postal_code as a German one
           res = self.germany.geocode(postal_code)
         end
-        # if nothing found, then it could be post box
-        # try to search by city
-        res = self.germany.geocode(city_name) if res.blank?
 
         # if still nothing found, use coordinates for Germany
         res = self.countries.geocode('DE') if res.blank?
---
+++ lib/simple/geocoder/austria.rb
@@ -0,0 +1,23 @@
+module Simple
+  module Geocoder
+    class Austria
+
+      attr_accessor :dict
+
+      def initialize
+        path = File.expand_path('../../../geodata/at_postal_codes.csv', __FILE__)
+        self.dict = {}
+        File.readlines(path).each do |line|
+          parts = line.strip.split(/\t+/)
+          self.dict[parts[1].strip] = {lat: parts[-3].strip.to_f, lon: parts[-2].strip.to_f}
+          self.dict[parts[2].strip] = {lat: parts[-3].strip.to_f, lon: parts[-2].strip.to_f}
+        end
+      end
+
+      def geocode postal_code_or_city
+        self.dict[postal_code_or_city]
+      end
+
+    end
+  end
+end
